6ae8bfb0d71d44a794f43b4de46541aeed2b8bf2
Slvvgsh <selvavignesh15700643@gmail.com>
2024-03-13 18:53:21 +0530

    Updates

---
+++ _next/static/TrM6UkmMGtCcvaxw4FS_P/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/427-46adbd3b280b33bc.js","static/chunks/pages/index-0b1852bfd71d5bed.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
