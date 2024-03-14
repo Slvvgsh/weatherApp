fc3d5c0796ef4431e3c93cb28e9e452d81373e77
Slvvgsh <selvavignesh15700643@gmail.com>
2024-03-14 12:03:01 +0530

    changes in the asset prefix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /*@type {import('next').NextConfig}*/ 
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: "/",
+  assetPrefix: "/weatherApp",
   output:'export',
   images:{
     unoptimized: true,
